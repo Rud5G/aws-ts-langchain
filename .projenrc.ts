c88f19c56cab67394763d735c61df4a5d3bec511
Martin Mueller <damadden88@googlemail.com>
2023-07-06 07:42:37 +0200

    init

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,9 +1,19 @@
 import { awscdk } from 'projen';
+import { TrailingComma } from 'projen/lib/javascript';
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.84.0',
   defaultReleaseBranch: 'main',
   name: 'aws-ts-langchain',
   projenrcTs: true,
+
+  eslint: true,
+  prettier: true,
+  prettierOptions: {
+    settings: {
+      singleQuote: true,
+      trailingComma: TrailingComma.ALL,
+    },
+  },
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
